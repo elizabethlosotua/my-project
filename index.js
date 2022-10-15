f3e092ab2bbe61b6e067d0848f26a870f52e76f6
elizabethlosotua <elizabethlosotua@gmail.com>
2022-10-15 11:24:50 +0300

    index JS

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-                                                                                                                                                            
+<button  onclick="fff()">Click Me!</button>
+
+
+function fff() {
+  let x = document.getElementById("fxnv");
+  x.style.fontSize = "25px";
+  x.style.color = "greed";
+}
+                                                                                                                                                        
